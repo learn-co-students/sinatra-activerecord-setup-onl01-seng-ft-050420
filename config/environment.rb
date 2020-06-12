771d8b585f28daad0866fde09df06c914fce0f1b
qu1xs <kevinnealmurphy@gmail.com>
2020-06-12 09:55:34 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ Bundler.require(:default, ENV['SINATRA_ENV'])
 
 configure :development do
     set :database, 'sqlite3:db/database.db'
-end
+  end
 
 require './app'
 
--- db/migrate/20200612134330_create_dogs.rb
+++ db/migrate/20200612134330_create_dogs.rb
@@ -1,11 +1,7 @@
 class CreateDogs < ActiveRecord::Migration[6.0]
-  def up
-    create_table :dogs |t|
+  def change
+    create_table :dogs do |t|
     t.string :name
     t.string :breed
-  end
-
-  def down
-    drop_table :dogs
   end
 end
