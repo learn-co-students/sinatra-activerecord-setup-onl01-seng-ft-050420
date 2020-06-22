97b4a24a9e52fd0a46e2f59a01cf7c187659bbb2
Irene Scott <irenejinscott@gmail.com>
2020-06-22 14:59:33 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,11 @@
 ENV['SINATRA_ENV'] ||= "development"
-
+#we should use the "development" environment for both the shotgun and the testing suite
+#we want to make sure that our migrations run on the same environment
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
-
+configure :development do
+  set :database, 'sqlite3:db/database.db'
+end
+#sets up a connection to sqlite3 database named database.db
+#rake gives us the ability to quickly make files and set up automated tasks
 require './app'
